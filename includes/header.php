2eff60fd25cb711482c5f0aa5a1ec555dda5e5ba
Nebojša Jarić <jaricn@gmail.com>
2020-07-30 14:11:03 +0200

    add javascript for future validation

--- includes/header.php
+++ includes/header.php
@@ -8,6 +8,41 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <?php
+        if ( ! empty( $_GET[ 'id' ] ) ) {
+            $id = intval( $_GET[ 'id' ] );
+            echo "<script>window.id = $id;</script>";
+        }
+    ?>
+    <script>
+        /**
+         * Validation for bootstrap
+         *
+         * @param field
+         * @param text - invalid text
+         * @param type - validation type
+         * @param field2 - second field (optional)
+         */
+        function customValidation( field, text, type, field2 ) {
+            field2 = field2 || false;
+
+            switch ( type ) {
+                case 'compare':
+                    type = field.value !== field2.value;
+                    break;
+                case 'dependency':
+                    type = ! field2.value && field.value;
+
+                    const swap = field2;
+
+                    field2 = field;
+                    field = swap;
+                    break;
+            }
+
+            field.setCustomValidity( type ? text : "" );
+        }
+    </script>
     <meta name="description" content="<?php echo $metas[ 'desc' ] ?>">
     <meta name="title" content="<?php echo $metas[ 'title' ] ?>">
     <meta name="keywords" content="<?php echo $metas[ 'keywords' ] ?>">
