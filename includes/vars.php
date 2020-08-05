1a67f20902dea59c756077d7fc9425b2e430b341
Nebojša Jarić <jaricn@gmail.com>
2020-08-05 11:38:16 +0200

    prevent 404 error for blank pages

--- includes/vars.php
+++ includes/vars.php
@@ -11,38 +11,38 @@ $keywords = [ 'ship-app', 'ships', 'test', 'portal' ];
 $page = empty( $_GET[ 'page' ] ) ? 'index' : $_GET[ 'page' ];
 $path = "./pages/$page.php";
 
-// Set 404 metas and path if page doesn't exist
-if ( ! is_file( $path ) ) {
-	$title            = 'Not Found';
-	$page             = '404';
-	$path             = './pages/404.php';
-} else {
-	// Set metas for other pages
-	switch ( $page ) {
-		default:
-			break;
-		case 'index':
-			$title = 'Main Page';
-			$desc = 'Listing of public user profiles.';
-			$path = FALSE;
+// Set metas for pages
+switch ( $page ) {
+	default:
+		$title = 'Not Found';
+		$desc = 'The requested page was not found.';
+		$page = '404';
+		$path = './pages/404.php';
 
-			array_push( $keywords, 'Welcome' );
-			break;
-		case 'admin':
-			$title = 'Administration Panel';
-			$desc = 'Administration dashboard.';
-			$path = FALSE;
+		array_push( $keywords, '404' );
+		array_push( $keywords, 'Not Found' );
+		break;
+	case 'index':
+		$title = 'Main Page';
+		$desc = 'Listing of public user profiles.';
+		$path = FALSE;
 
-			array_push( $keywords, 'Administration' );
-			break;
-		case 'login':
-			$title = 'Log in';
-			$desc = 'Log in to our portal.';
+		array_push( $keywords, 'Welcome' );
+		break;
+	case 'admin':
+		$title = 'Administration Panel';
+		$desc = 'Administration dashboard.';
+		$path = FALSE;
 
-			array_push( $keywords, 'Authentication' );
-			array_push( $keywords, 'Login' );
-			break;
-	}
+		array_push( $keywords, 'Administration' );
+		break;
+	case 'login':
+		$title = 'Log in';
+		$desc = 'Log in to our portal.';
+
+		array_push( $keywords, 'Authentication' );
+		array_push( $keywords, 'Login' );
+		break;
 }
 
 $metas = [
