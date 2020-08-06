cb1dfacebcc867cdc8e400a1d4f110b649b945bd
Nebojša Jarić <jaricn@gmail.com>
2020-08-06 10:36:42 +0200

    add image upload form

--- pages/newShip.php
+++ pages/newShip.php
@@ -1,8 +1,20 @@
 <form id="ship" class="auth-form needs-validation" novalidate>
     <div id="soft-error" class="d-none pb-2 text-danger"></div>
+    <input type="hidden" id="id" name="id" value="<?php if ( isset( $id ) ) echo $id ?>" />
+    <input type="hidden" id="image_url" name="image_url" />
+    <div class="form-group">
+        <label for="image">Your Avatar</label>
+        <input type="file" class="form-control d-none" id="image" name="image" accept="image/*" >
+        <br>
+        <div id="image_container" class="p-1 border d-inline-block">
+            <div id="image_canvas">
+                <img id="output_image" src="<?php echo $assets ?>images/placeholder.png" alt="Your avatar" />
+            </div>
+        </div>
+    </div>
 	<div class="form-group">
 		<label for="name">Name</label>
-		<input type="text" class="form-control" id="name" name="name" placeholder="Enter rank name..." required maxlength="250">
+		<input type="text" class="form-control" id="name" name="name" placeholder="Enter ship name..." required maxlength="250">
         <div class="invalid-feedback">
             Please provide a valid ship name.
         </div>
@@ -14,6 +26,5 @@
             Please provide a valid serial number.
         </div>
     </div>
-    <!--TODO image upload-->
 	<button type="submit" class="btn btn-primary">Save</button>
 </form>
