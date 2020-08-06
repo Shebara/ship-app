396f8acdc1e4fda63840e1b9ef91b299f68ab61d
Nebojša Jarić <jaricn@gmail.com>
2020-08-06 12:02:29 +0200

    disable editing admin rank

--- pages/newRank.php
+++ pages/newRank.php
@@ -1,12 +1,17 @@
+<?php
+$id = isset( $id ) ? $id : FALSE;
+$disabled = $id == 1;
+?>
 <form id="rank" class="auth-form needs-validation" novalidate>
     <div id="soft-error" class="d-none pb-2 text-danger"></div>
-    <input type="hidden" id="id" name="id" value="<?php if ( isset( $id ) ) echo $id ?>" />
+    <input type="hidden" id="id" name="id" value="<?php if ( $id ) echo $id ?>" />
 	<div class="form-group">
 		<label for="name">Rank Name</label>
-		<input type="text" class="form-control" id="name" name="name" placeholder="Enter rank name..." required maxlength="250">
+		<input type="text" class="form-control" id="name" name="name" placeholder="Enter rank name..." required
+               maxlength="250" <?php if ( $disabled ) echo 'disabled' ?>>
         <div class="invalid-feedback">
             Please provide a valid rank name.
         </div>
 	</div>
-	<button type="submit" class="btn btn-primary">Save</button>
+	<button type="submit" class="btn btn-primary" <?php if ( $disabled ) echo 'disabled' ?>>Save</button>
 </form>
